870f62fe45127c22e1daa6d78ca0d425cf3a048e
58GANJI-CORP\liudongxu06 <liudongxu06@58ganji.com>
2018-01-04 13:56:16 +0800

    first commit

---
+++ chapter1/Client/src/main/java/demo/netty/chapter1/echoclient/EchoClientHandler.java
@@ -0,0 +1,31 @@
+package demo.netty.chapter1.echoclient;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author liudongxu06
+ * @date 2017/12/29
+ */
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Nettry client echo",CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("Client reveived:"+byteBuf.toString(CharsetUtil.UTF_8));
+    }
+}
